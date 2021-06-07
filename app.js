ea4b29f1751c0d6dd032045d75623f0fa3fa91db
GHADEER KHASAWNEH <GHADEER_KHASAWNEH@YAHOO.COM>
2021-06-08 02:39:31 +0300

    editing lab11

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-let arrOfObjects = [];
+
 
 let randomIndexNumberOne;
 let randomIndexNumberTwo;
 let randomIndexNumberThree;
 
 
-
+let section = document.getElementById('sectionOne');
 let firstImage = document.getElementById('imageOne');
 let secondImage = document.getElementById('imageTwo');
 let thirdImage = document.getElementById('imageThree');
 
 
-let selectionTimes = 10;
+let selectionTimes = 25;
 let numberOfClicks = 0;
+
+let arrOfNames = [];
+let arrOfVotes = [];
 
 
 function images (productName, imagePath){
@@ -25,29 +28,33 @@ function images (productName, imagePath){
 
 }
 
+arrOfObjects = [];
 
 
-new images = ('bag', 'img/bag.jpg');
-new images = ('banana', 'img/banana.jpg');
-new images = ('bathroom', 'img/bathroom.jpg');
-new images = ('boots', 'img/boots/jpg');
-new images = ('breakfast', 'img/breakfast.jpg');
-new images = ('bubblegum', 'img/bubblegum.jpg');
-new images = ('chair', 'img/chair.jpg');
-new images = ('cthulhu', 'img/cthulhu.jpg');
-new images = ('dog-duck', 'img/dog-duck.jpg');
-new images = ('dragon', 'img/dragon.jpg');
-new images = ('pen', 'img/pen.jpg');
-new images = ('pet-sweep', 'img/pet-sweep.jpg');
-new images = ('scissors', 'img/scissors.jpg');
-new images = ('shark', 'img/shark.jpg');
-new images = ('sweep', 'img/sweep.png');
-new images = ('tauntaun', 'img/tauntaun.jpg');
-new images = ('unicorn', 'img/unicorn.jpg');
-new images = ('usb', 'img/usb.gif');
-new images = ('water-can', 'img/water-can.jpg');
-new images = ('wine-glass', 'img/wine-glass.jpg');
 
+new images ('bag', 'img/bag.jpg');
+new images  ('banana', 'img/banana.jpg');
+new images  ('bathroom', 'img/bathroom.jpg');
+new images  ('boots', 'img/boots.jpg');
+new images  ('breakfast', 'img/breakfast.jpg');
+new images  ('bubblegum', 'img/bubblegum.jpg');
+new images ('chair', 'img/chair.jpg');
+new images  ('cthulhu', 'img/cthulhu.jpg');
+new images  ('dog-duck', 'img/dog-duck.jpg');
+new images  ('dragon', 'img/dragon.jpg');
+new images  ('pen', 'img/pen.jpg');
+new images  ('pet-sweep', 'img/pet-sweep.jpg');
+new images  ('scissors', 'img/scissors.jpg');
+new images  ('shark', 'img/shark.jpg');
+new images  ('sweep', 'img/sweep.png');
+new images  ('tauntaun', 'img/tauntaun.jpg');
+new images  ('unicorn', 'img/unicorn.jpg');
+new images  ('usb', 'img/usb.gif');
+new images  ('water-can', 'img/water-can.jpg');
+new images ('wine-glass', 'img/wine-glass.jpg');
+
+
+console.log(arrOfObjects);
 
 function randomNumber (){
 
@@ -55,7 +62,7 @@ function randomNumber (){
 
     return randomValue;
 
-}
+};
 
 
 
@@ -66,30 +73,34 @@ function generateAndDisplay (){
     randomIndexNumberTwo = randomNumber();
     randomIndexNumberThree = randomNumber();
 
-if (randomIndexNumberOne === randomIndexNumberTwo){
-    randomIndexNumberOne = randomNumber
-}
-else if (randomIndexNumberOne === randomIndexNumberThree){
-    randomIndexNumberOne = randomNumber
-}
-else if ( randomIndexNumberTwo === randomIndexNumberThree){
-    randomIndexNumberTwo
-}
+    if (randomIndexNumberOne === randomIndexNumberTwo){
+        randomIndexNumberOne = randomNumber();
+    }
+    else if (randomIndexNumberOne === randomIndexNumberThree){
+        randomIndexNumberOne = randomNumber();
+    }
+    else if ( randomIndexNumberTwo === randomIndexNumberThree){
+        randomIndexNumberTwo = randomNumber();
+    }
 
 
-    // imageOne = imagePath + randomIndexNumberOne;
     firstImage.src = arrOfObjects[randomIndexNumberOne].imagePath;
+    arrOfObjects[randomIndexNumberOne].shown++;
+
     secondImage.setAttribute('src', arrOfObjects[randomIndexNumberTwo].imagePath);
-    thirdImage.setAttribute('src', arrOfObjects[randomIndexNumberTwo].imagePath);
+    arrOfObjects[randomIndexNumberTwo].shown++;
+    
+    thirdImage.setAttribute('src', arrOfObjects[randomIndexNumberThree].imagePath);
+    arrOfObjects[randomIndexNumberThree].shown++;
+
 
 
 }
 
 generateAndDisplay();
 
-firstImage.addEventListener('click', clicking);
-secondImage.addEventListener('click', clicking);
-thirdImage.addEventListener('click', clicking);
+section.addEventListener('click', clicking);
+let buttonElement;
 
 
 function clicking(event){
@@ -104,23 +115,54 @@ function clicking(event){
             arrOfObjects[randomIndexNumberTwo].votes++;
         }else if (event.target.id === 'imageThree'){
             arrOfObjects[randomIndexNumberThree].votes++;
+        }else {
+            numberOfClicks--;
+            return
         }
-
         
-    generateAndDisplay();
+        generateAndDisplay();
 
 
-    }
+    }else {
+        buttonElement = document.getElementById('btn');
+        buttonElement.addEventListener('click', showItems);
+        section.removeEventListener('click', clicking);
+    } 
 
-    else {
-        createList();
-        firstImage.removeEventListener('click', clicking);
-        secondImage.removeEventListener('click', clicking);
-        thirdImage.removeEventListener('click', clicking);
-    }
    
 }
 
-function createList(){
-    
+
+let array =  [];
+
+function showItems(){
+showList();
+
+buttonElement.removeEventListener('click', showItems);
+
 }
+
+let arrOfShownItems = [];
+
+function showList(){
+    let unOrderedList = document.getElementById('list');
+    for(let i = 0; i < arrOfObjects.length; i++){
+        arrOfVotes.push(arrOfObjects[i].votes);
+
+        arrOfShownItems.push(arrOfObjects[i].shown);
+        arrOfNames.push(arrOfObjects[i].productName);
+
+
+        let li =document.createElement('li');
+        unOrderedList.appendChild(li);
+        li.textContent = `${arrOfObjects[i].productName} had ${arrOfObjects[i].votes} votes and was shown ${arrOfObjects[i].shown}`;
+
+    }
+}
+
+
+
+
+
+console.log(arrOfNames);
+
